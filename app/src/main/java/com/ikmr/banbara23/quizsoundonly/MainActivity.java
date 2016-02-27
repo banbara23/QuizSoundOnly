2c87b350551f9072d4da7cbd99a0077298df9346
池村 和剛 <kazutakai@tt-com.net>
2016-02-27 10:18:34 +0900

    実装

--- app/src/main/java/com/ikmr/banbara23/quizsoundonly/MainActivity.java
+++ app/src/main/java/com/ikmr/banbara23/quizsoundonly/MainActivity.java
@@ -1,13 +1,80 @@
+
 package com.ikmr.banbara23.quizsoundonly;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initAdView();
+
+        initButton();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+    }
+
+    private void initAdView() {
+        mAdView = (AdView) findViewById(R.id.adView);
+        if (mAdView == null) {
+            return;
+        }
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    private void initButton() {
+        findViewById(R.id.question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer.create(getApplicationContext(), R.raw.question).start();
+            }
+        });
+
+        findViewById(R.id.correct).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer.create(getApplicationContext(), R.raw.correct).start();
+            }
+        });
+        findViewById(R.id.fault).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer.create(getApplicationContext(), R.raw.wrong).start();
+            }
+        });
     }
 }
